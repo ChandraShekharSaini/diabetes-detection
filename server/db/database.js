b8a9606149fad4efca216f43a07d781733809ec1
Chandra Shekhar Saini <chandrashekharsaini322@gmail.com>
2025-11-01 19:41:29 +0530

    Added mongodb

--- server/db/database.js
+++ server/db/database.js
@@ -1,21 +1,20 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config()
 
-export const DB = () => {
+dotenv.config();
 
-
-  
-  mongoose
-    .connect("mongodb+srv://chandrashekharsaini322_db_user:red123@glucoai.kv4gm2r.mongodb.net/?appName=GlucoAI")
-    .then(() => {
-      console.log("DB Conected");
-
-      
-  console.log("Google Sectect",process.env.GOOGLE_CLIENT_SECRET);
-  console.log("gOOGLE cALLBACK",process.env.GOOGLE_CALLBACK_URL);
-    })
-    .catch((error) => {
-      console.log(error.message);
+export const DB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, 
     });
+
+    console.log("MongoDB Connected Successfully",process.env.MONGO_URI,);
+    console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+    console.log("GOOGLE_CALLBACK_URL:", process.env.GOOGLE_CALLBACK_URL);
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error.message);
+  }
 };
