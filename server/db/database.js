c2736b88e123eb4e0c995769de393d53408aecd7
Chandra Shekhar Saini <chandrashekharsaini322@gmail.com>
2025-11-01 19:46:37 +0530

    Added MongoDb Uri

--- server/db/database.js
+++ server/db/database.js
@@ -5,13 +5,13 @@ dotenv.config();
 
 export const DB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000, 
     });
 
-    console.log("MongoDB Connected Successfully",process.env.MONGO_URI,);
+    console.log("MongoDB Connected Successfully",process.env.MONGODB_URI,);
     console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
     console.log("GOOGLE_CALLBACK_URL:", process.env.GOOGLE_CALLBACK_URL);
   } catch (error) {
