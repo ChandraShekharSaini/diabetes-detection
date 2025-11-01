7be971d89b1eaf35df3eadbb54f3954ebacd2f8e
Chandra Shekhar Saini <chandrashekharsaini322@gmail.com>
2025-11-01 19:28:27 +0530

    Added Files

--- server/db/database.js
+++ server/db/database.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export const DB = () => {
+
+
   
   mongoose
     .connect("mongodb+srv://chandrashekharsaini322_db_user:red123@glucoai.kv4gm2r.mongodb.net/?appName=GlucoAI")
     .then(() => {
       console.log("DB Conected");
+
+      
+  console.log("Google Sectect",process.env.GOOGLE_CLIENT_SECRET);
+  console.log("gOOGLE cALLBACK",process.env.GOOGLE_CALLBACK_URL);
     })
     .catch((error) => {
       console.log(error.message);
